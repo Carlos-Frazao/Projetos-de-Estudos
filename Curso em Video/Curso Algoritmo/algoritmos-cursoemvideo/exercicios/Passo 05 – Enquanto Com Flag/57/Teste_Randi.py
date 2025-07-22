119682afad2cecf166a35ea77de3c5d617f3d6c2
Carlos-Frazao <ce416669@gmail.com>
2025-07-21 21:59:20 -0300

    Projeto de Estudos

---
+++ Curso em Video/Curso Algoritmo/algoritmos-cursoemvideo/exercicios/Passo 05 – Enquanto Com Flag/57/Teste_Randi.py
@@ -0,0 +1,7 @@
+def randomizando_numero(numero):
+    import random 
+    return random.randint(1, 100)
+
+n = int(input('Digite um número: '))
+print(f"O número que você digitou foi, {n} e o número aleatório gerado foi {randomizando_numero(n)}")
+#Teste

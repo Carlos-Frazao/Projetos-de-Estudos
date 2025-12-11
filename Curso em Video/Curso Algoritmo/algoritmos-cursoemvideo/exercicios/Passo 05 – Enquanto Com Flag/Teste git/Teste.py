e8dff1d2f487da458bda685b632b4cdf45140bd1
Carlos-Frazao <ce416669@gmail.com>
2025-12-11 12:36:09 -0300

    Projeto de Estudos

--- Curso em Video/Curso Algoritmo/algoritmos-cursoemvideo/exercicios/Passo 05 – Enquanto Com Flag/Teste git/Teste.py
+++ Curso em Video/Curso Algoritmo/algoritmos-cursoemvideo/exercicios/Passo 05 – Enquanto Com Flag/Teste git/Teste.py
@@ -1,2 +1,23 @@
-import this 
-print("Hello world! ")
+# Função principal 
+def main():
+
+    # Criando uma função para cálcular a média
+    def calculo_media(x, y):
+        #x = soma das notas 
+        #y = qtd de notas
+        return x / y
+
+    # Fazendo um pograma que cálcula a média, com função 
+    qtd_notas = int(input("Quantas notas: "))
+
+    c = 1
+    soma = 0
+    while (qtd_notas >= c):
+        nota = float(input(f"Digite a nota {c}° : "))
+        soma = soma + nota
+        c += 1
+
+    print(f"A média das notas é  {calculo_media(soma, qtd_notas)}")
+
+if __name__ == "__main__":
+    main()

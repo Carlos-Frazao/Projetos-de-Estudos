e19be3e67e37a5b28fa4ff1333011dfc7274813e
Carlos-Frazao <ce416669@gmail.com>
2026-01-18 02:37:05 -0300

    Projeto de Estudos

--- Curso em Video/Curso Algoritmo/algoritmos-cursoemvideo/exercicios/Passo 05 – Enquanto Com Flag/57/Teste_Randi.py
+++
@@ -1,8 +0,0 @@
-def randomizando_numero(numero):
-    import random 
-    return random.randint(1, 100)
-
-n = int(input('Digite um número: '))
-print(f"O número que você digitou foi, {n} e o número aleatório gerado foi {randomizando_numero(n)}")
-#Teste
-#Teste
--- Curso em Video/Curso Algoritmo/algoritmos-cursoemvideo/exercicios/Passo 05 – Enquanto Com Flag/Teste git/whili.py
+++
@@ -1,4 +0,0 @@
-c = 0 
-while c < 10:
-    print(c)
-    c += 1 

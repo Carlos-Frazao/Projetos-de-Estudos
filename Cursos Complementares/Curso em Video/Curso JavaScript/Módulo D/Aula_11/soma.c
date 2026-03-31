2f9809ef0c024fbd8bc41eb8706fddae8b31e7a6
Carlos Eduardo <ce416669@gmail.com>
2026-03-30 23:39:07 -0300

    Código em C teste

---
+++ Cursos Complementares/Curso em Video/Curso JavaScript/Módulo D/Aula_11/soma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n1, n2;
+
+    printf("Informe um valor: ");
+    scanf("%d", &n1);
+    printf("Informe outro valor: ");
+    scanf("%d", &n2);
+
+    printf("A soma de %d e %d é igual a %d: ",n1, n2, n1+n2);
+
+
+    return 0;
+}
+
+Rapaz
---
+++ Cursos Complementares/Curso em Video/Curso JavaScript/Módulo D/Aula_11/soma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    cout << "Informe um valor inteiro:";
+    cin >> n1;
+    cout << "Informe outro valor inteiro:";
+    cin >> n2;
+
+    cout << "A soma de " << n1 << " e " << n2 << " é igual a " << n1 + n2;
+}
